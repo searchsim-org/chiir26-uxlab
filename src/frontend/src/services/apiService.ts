8ffd5425e3ceeebb4ca0cad1c3208aaac109299e
Saber Zerhoudi <saber.zerhoudi@uni-passau.de>
2025-11-07 01:20:45 +0100

    Updated version

--- src/frontend/src/services/apiService.ts
+++ src/frontend/src/services/apiService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with the base URL
 const apiService = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8100',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://uxlab.searchsim.org',
 });
 
 // Function to load survey questions
